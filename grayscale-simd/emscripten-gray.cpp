14eb0b4612e727e349e3f6e3fdd4fe6577a4364c
kalwalt <github@kalwaltart.it>
2024-06-11 17:10:06 +0200

    samll improves in the grayscale simd example

--- grayscale-simd/emscripten-gray.cpp
+++ grayscale-simd/emscripten-gray.cpp
@@ -7,9 +7,11 @@
 emscripten::val convert_to_gray(emscripten::val img, int width, int height, int channels, int threads) 
 {
     auto u8_img = emscripten::convertJSArrayToNumberVector<uint8_t>(img);
-    auto result = convert_memory_simd_sse(u8_img.data(), width, height, channels, threads);
-    return emscripten::val(emscripten::typed_memory_view(width*height*1, result));
-    delete[] result;
+    auto out = convert_memory_simd_sse(u8_img.data(), width, height, channels, threads);
+    emscripten::val view{emscripten::typed_memory_view(width*height*1, out)};
+    auto result = emscripten::val::global("Uint8Array").new_(width*height*1);
+    result.call<void>("set", view);
+    return view;
 }
 
 EMSCRIPTEN_BINDINGS(gray_emscripten) {
