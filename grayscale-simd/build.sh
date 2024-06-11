5cc65aeca971afa72e0dfe6e35e1810f0c088eee
kalwalt <github@kalwaltart.it>
2024-06-11 17:10:06 +0200

    samll improves in the grayscale simd example

--- grayscale-simd/build.sh
+++ grayscale-simd/build.sh
@@ -1,2 +1,4 @@
-emcc gray.cpp -mavx -msimd128 -sEXPORTED_FUNCTIONS=convert_memory_simd_sse -r -o gray.bc
-emcc gray.bc emscripten-gray.cpp -s ALLOW_MEMORY_GROWTH=1 -O3 --bind -o gray.js
+em++ gray.cpp -mavx -msimd128 -sEXPORTED_FUNCTIONS=convert_memory_simd_sse -r -o gray.bc
+# em++ gray.cpp -mavx -msimd128 -sEXPORTED_FUNCTIONS=convert_memory_simd_sse -s SAFE_HEAP=1 -gsource-map -sASSERTIONS=2 --profiling-funcs -r -o gray.bc
+em++ gray.bc emscripten-gray.cpp -s ALLOW_MEMORY_GROWTH=1 -Oz --bind -o gray.js
+# em++ gray.bc emscripten-gray.cpp -sEXPORTED_FUNCTIONS=convert_memory_simd_sse -s ALLOW_MEMORY_GROWTH=1 -s SAFE_HEAP=1 -gsource-map -sASSERTIONS=2 --profiling-funcs --bind -o gray.js
--- grayscale-simd/main.js
+++ grayscale-simd/main.js
@@ -1,7 +1,6 @@
 Module.onRuntimeInitialized = async function(){
     console.log(Module);
     var bytearray = new Uint8Array([255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255]);
-    //var res = new Uint8Array(12);
     var res = Module.convert_to_gray(bytearray, 2, 2, 3, 1)
-    console.log(res);
+    console.log("result from convert_to_gray: ", res);
     }
