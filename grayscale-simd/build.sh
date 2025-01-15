6de65221f22ba243334adffeba19b1c5140f2b21
Walter Perdan <github@kalwaltart.it>
2025-01-15 16:10:04 +0100

    testing with the pinball image

--- grayscale-simd/build.sh
+++ grayscale-simd/build.sh
@@ -1,5 +1,1 @@
-emcc gray.cpp -I./ -mavx -msimd128 -sEXPORTED_FUNCTIONS=convert_memory_simd_sse -r -o gray.bc
-emcc gray.bc emscripten-gray.cpp -s ALLOW_MEMORY_GROWTH=1 -Oz --bind -o gray.js
-
-#final correct build command:
-emcc -I./ -pthread -mavx -msimd128 emscripten-gray.cpp -s ALLOW_MEMORY_GROWTH=1 -Oz -sWASM=1 --bind -o gray.js
+emcc -I./ -mavx -msimd128 emscripten-gray.cpp -s ALLOW_MEMORY_GROWTH=1 -Oz -sWASM=1 --bind -o gray.js
