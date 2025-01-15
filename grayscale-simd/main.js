c6ca3f782918409439a97f92e6576f745f482d42
Walter Perdan <github@kalwaltart.it>
2025-01-15 15:40:57 +0100

    memory_simd_sse gray.cpp

--- grayscale-simd/main.js
+++ grayscale-simd/main.js
@@ -1,6 +1,6 @@
 Module.onRuntimeInitialized = async function(){
     console.log(Module);
     var bytearray = new Uint8Array([255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255]);
-    var res = Module.convert_to_gray(bytearray, 2, 2, 3, 1)
+    var res = Module.convert_to_gray(bytearray, 2, 2, 3, 4)
     console.log("Result from convert_to_gray function: ", res);
     }
