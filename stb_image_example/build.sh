4fed9fea19ead6022659baa1bf405bf11fdabec9
Walter Perdan <github@kalwaltart.it>
2025-02-04 20:19:50 +0100

    cmake option to build the project

--- stb_image_example/build.sh
+++ stb_image_example/build.sh
@@ -1,1 +1,5 @@
 emcc simple_stb_example.cpp -I./stb_image -sWASM=1 -std=c++17 --embed-file chalk.jpg --bind -o stb_example.js
+
+# to build with emcmake use these commands in a console
+# cmake -G "Unix Makefiles" -B build -S ./ ..
+# emmake make -C build
