da0edcefd54939a93a40a122d60c9f8879311d8c
Walter Perdan <github@kalwaltart.it>
2025-02-04 21:08:29 +0100

    minor fixes

--- stb_image_example/build.sh
+++ stb_image_example/build.sh
@@ -1,7 +1,7 @@
 emcc simple_stb_example.cpp -I./stb_image -sWASM=1 -std=c++17 --embed-file chalk.jpg --bind -o stb_example.js
 
 # to build with emcmake use these commands in a console
-# cmake -G "Unix Makefiles" -B build -S ./ ..
+# emcmake cmake -G "Unix Makefiles" -B build -S ./ ..
 # emmake make -C build
 # cp build/stb_example.js stb_example.js
 # cp build/stb_example.wasm stb_example.wasm
